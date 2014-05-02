f09a4b22d94c06b1b894295d9c853655c9e993c0
rakesh456 <rakesh.itatyourservice@gmail.com>
2014-05-02 20:10:11 +0530

    Create getScrollOffsets.js

---
+++ getScrollOffsets.js
@@ -0,0 +1,17 @@
+// Return the current scrollbar offsets as the x and y properties of an object
+// Author: David Flanagan in his book JavaScript The Definitive Guide Sixth Edition, Example 15-8
+function getScrollOffsets(w) {
+	// Use the specifies window or the current window if no argument
+	w = w || window;
+	
+	// This works for all browsers except IE versions 8 and before
+	if (w.pageXOffset != null) return {x: w.pageXOffset, y:w.pageYOffset};
+	
+	// For IE (or any browser) in Standards mode
+	var d = w.document;
+	if (document.compatMode == "CSS1Compat")
+		return {x:d.documentElement.scrollLeft, y:d.documentElement.scrollTop};
+	
+	// For browsers in Quirks mode
+	return { x: d.body.scrollLeft, y: d.body.scrollTop };
+}
