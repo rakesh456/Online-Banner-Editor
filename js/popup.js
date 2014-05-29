2159491a318e61169fe362962f607499a5707415
rakesh456 <rakesh.itatyourservice@gmail.com>
2014-05-30 00:04:11 +0530

    Removed files not in use.

--- js/popup.js
+++ js/popup.js
@@ -7,8 +7,8 @@ POP.add({title: 'title:', function: 'custom_function'});
 
 POP.show('title', main_handler, 'preview_handler', 'onload_handler');
 */
-var POP = new popup();
 
+var POP = new popup();
 function popup(){
 	this.active = false;
 	this.handler = '';
@@ -16,12 +16,9 @@ function popup(){
 	this.onload = false;
 	this.width_mini = 195;
 	this.height_mini = 195;
-	this.preview_in_main = false;
 	this.id = 0;
 	var parameters = [];
-	var layer_active_small = document.createElement("canvas");
-	var layer_active_small_ctx = layer_active_small.getContext("2d");
-	
+
 	this.constructor = new function(){
 		var dim = HELPER.get_dimensions();
 		popup = document.getElementById('popup');
@@ -67,8 +64,6 @@ function popup(){
 						onchange = ' onchange="'+parameter.onchange+';" ';
 					if(parameter.values.length > 10 || parameter.type == 'select'){
 						//drop down
-						if(onchange == '' && preview_handler != undefined)
-							onchange = ' onchange="POP.view();" ';
 						html += '<td colspan="2"><select '+onchange+' style="font-size:12px;" id="pop_data_'+parameter.name+'">';
 						var k = 0;
 						for(var j in parameter.values){
@@ -94,8 +89,6 @@ function popup(){
 								ch = 'checked="checked"';
 							if(parameter.value == undefined && k == 0)
 								ch = 'checked="checked"';
-							if(onchange == '' && preview_handler != undefined)
-								onchange = ' onchange="POP.view();" ';
 							html += '<input type="radio" '+onchange+' '+ch+' name="'+parameter.name+'" id="pop_data_'+parameter.name+"_poptmp"+j+'" value="'+parameter.values[j]+'">';
 							html += '<label style="margin-right:20px;" for="pop_data_'+parameter.name+"_poptmp"+j+'">'+parameter.values[j]+'</label>';
 							if(parameter.values.length > 2)
@@ -112,10 +105,7 @@ function popup(){
 					if(parameter.step != undefined)
 						step = parameter.step;
 					if(parameter.range != undefined){
-						var preview_code = '';
-						if(this.preview !== false)
-							preview_code = 'POP.view();';
-						html += '<td><input type="range" id="pop_data_'+parameter.name+'" value="'+parameter.value+'" min="'+parameter.range[0]+'" max="'+parameter.range[1]+'" step="'+step+'" " oninput="document.getElementById(\'pv'+i+'\').innerHTML=Math.round(this.value*100)/100;'+preview_code+'" /></td>';
+						html += '<td><input type="range" id="pop_data_'+parameter.name+'" value="'+parameter.value+'" min="'+parameter.range[0]+'" max="'+parameter.range[1]+'" step="'+step+'" " oninput="document.getElementById(\'pv'+i+'\').innerHTML=Math.round(this.value*100)/100;" /></td>';
 						html += '<td style="padding-left:10px;width:50px;" id="pv'+i+'">'+parameter.value+'</td>';
 						}
 					else{
@@ -151,18 +141,10 @@ function popup(){
 			html += '</tr>';
 			}
 		html += '</table>';
-		if(this.preview !== false && this.preview_in_main == false){
-			html += '<div style="margin-top:15px;">';
-			html += '<canvas style="position:relative;float:left;margin-right:5px;border:1px solid #393939;" width="'+POP.width_mini+'" height="'+POP.height_mini+'" id="pop_pre"></canvas>';
-			html += '<canvas style="position:relative;border:1px solid #393939;background-color:#ffffff;" width="'+POP.width_mini+'" height="'+POP.height_mini+'" id="pop_post"></canvas>';
-			html += '</div>';
-			}
 		html += '<div style="text-align:center;margin-top:20px;margin-bottom:15px;">';
 		html += '<input type="button" onclick="POP.save();" class="button" value="OK" />';
 		if(can_be_canceled==true)
 			html += '<input type="button" onclick="POP.hide();" class="button" value="Cancel" />';
-		if(this.preview !== false)
-			html += '<input type="button" onclick="POP.view();" class="button" value="Preview" />';	
 		html += '</div>';
 			
 		document.getElementById("popup").innerHTML = html;
@@ -180,30 +162,6 @@ function popup(){
 				this.onload();
 			}
 		
-		//load preview?
-		if(this.preview !== false && this.preview_in_main == false){
-			//original
-			var pop_pre = document.getElementById("pop_pre").getContext("2d");
-			pop_pre.rect(0, 0, POP.width_mini, POP.height_mini);
-			pop_pre.fillStyle = "#ffffff";
-			pop_pre.fill();
-			DRAW.draw_background(pop_pre, POP.width_mini, POP.height_mini, 5);
-			pop_pre.drawImage(document.getElementById(LAYERS[LAYER.layer_active].name), 0, 0, POP.width_mini, POP.height_mini);
-			
-			//copy
-			pop_post = document.getElementById("pop_post").getContext("2d");
-			pop_post.rect(0, 0, POP.width_mini, POP.height_mini);
-			pop_post.fillStyle = "#ffffff";
-			pop_post.fill();
-			DRAW.draw_background(pop_post, POP.width_mini, POP.height_mini, 5);
-			pop_post.drawImage(document.getElementById(LAYERS[LAYER.layer_active].name), 0, 0, POP.width_mini, POP.height_mini);
-			
-			//prepare temp canvas
-			layer_active_small.width = POP.width_mini;
-			layer_active_small.height = POP.height_mini;
-			layer_active_small_ctx.drawImage(document.getElementById(LAYERS[LAYER.layer_active].name), 0, 0, POP.width_mini, POP.height_mini);	
-			POP.view();
-			}
 		};
 	//hide popup
 	this.hide = function(){
@@ -215,56 +173,6 @@ function popup(){
 		this.onload = false;
 		this.preview_in_main = false;
 		canvas_front.clearRect(0, 0, WIDTH, HEIGHT);
-		};
-	//renders preview. If input=range supported, is called on every param update - must be fast...
-	this.view = function(){
-		if(this.preview !== false){
-			if(this.preview_in_main == false){
-				//reset mini view
-				pop_post.clearRect(0, 0, POP.width_mini, POP.height_mini);
-				pop_post.drawImage(layer_active_small, 0, 0);
-				}
-				
-			//prepare
-			var response = {};
-			inputs = document.getElementsByTagName('input');
-			for (i = 0; i<inputs.length; i++){
-				if(inputs[i].id.substr(0,9)=='pop_data_'){
-					var key = inputs[i].id.substr(9);
-					if(HELPER.strpos(key, "_poptmp") != false)
-						key = key.substring(0, HELPER.strpos(key, "_poptmp"));
-					var value = inputs[i].value;
-					if(inputs[i].type == 'radio'){
-						if(inputs[i].checked==true)
-							response[key] = value;
-						}
-					else
-						response[key] = value;
-					}
-				}
-			selects = document.getElementsByTagName('select');
-			for (i = 0; i<selects.length; i++) {
-				if(selects[i].id.substr(0,9)=='pop_data_'){
-					var key = selects[i].id.substr(9);
-					var value = selects[i].value;
-					response[key] = value;
-					}
-				}
-			textareas = document.getElementsByTagName('textarea');
-			for (i = 0; i<textareas.length; i++){
-				if(textareas[i].id.substr(0,9)=='pop_data_'){
-					var key = textareas[i].id.substr(9);
-					var value = textareas[i].value;
-					response[key] = value;
-					}
-				}
-			
-			//call handler
-			if(this.preview_in_main == false)
-				this.preview(response, pop_post, POP.width_mini, POP.height_mini);
-			else
-				this.preview(response);
-			}
 		};
 	//OK pressed - prepare data and call handlers
 	this.save = function(){
@@ -287,24 +195,7 @@ function popup(){
 				
 				}
 			}
-		selects = document.getElementsByTagName('select');
-		for (i = 0; i<selects.length; i++) {
-			if(selects[i].id.substr(0,9)=='pop_data_'){
-				var key = selects[i].id.substr(9);
-				var value = selects[i].value;
-				response[key] = value;
-				}
-			}
-		textareas = document.getElementsByTagName('textarea');
-		for (i = 0; i<textareas.length; i++){
-			if(textareas[i].id.substr(0,9)=='pop_data_'){
-				var key = textareas[i].id.substr(9);
-				var value = textareas[i].value;
-				response[key] = value;
-				}
-			}
-		parameters = [];
-		this.preview = false;
+		parameters = [];		
 		this.onload = false;
 		if(this.handler != ''){
 			if(typeof this.handler == "string")
